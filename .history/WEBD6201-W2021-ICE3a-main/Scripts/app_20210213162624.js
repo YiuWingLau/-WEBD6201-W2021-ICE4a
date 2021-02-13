a39d26657d961d2bd6d733d7823e4e408a6d8e87
Yiu Wing Lau <yiuwing.lau@dcmail.ca>
2021-02-13 16:46:55 -0500

    added login and register pages with css

--- .history/WEBD6201-W2021-ICE3a-main/Scripts/app_20210213162624.js
+++ .history/WEBD6201-W2021-ICE3a-main/Scripts/app_20210213162624.js
@@ -74,75 +74,23 @@
 
     }
 
-    function testFullName()
-    {
-      let messageArea = $("#messageArea").hide();
-      let fullNamePattern = /([A-Z][a-z]{1,25})+(\s|,|-)([A-Z][a-z]{1,25})+(\s|,|-)*/;
-
-        
-        $("#fullName").on("blur", function()
-        {
-          if(!fullNamePattern.test($(this).val()))
-          {
-            $(this).trigger("focus").trigger("select");
-            messageArea.show().addClass("alert alert-danger").text("Please enter a valid Full Name. This must include at least a Capitalized first name followed by a Capitlalized last name.");
-          }
-          else
-          {
-              messageArea.removeAttr("class").hide();
-          }
-        });
-    }
-
-    function testContactNumber()
-    {
-      let messageArea = $("#messageArea");
-      let contactNumberPattern = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
-        
-        $("#contactNumber").on("blur", function()
-        {
-          if(!contactNumberPattern.test($(this).val()))
-          {
-            $(this).trigger("focus").trigger("select");
-            messageArea.show().addClass("alert alert-danger").text("Please enter a valid Contact Number. Country code and area code are both optional");
-          }
-          else
-          {
-              messageArea.removeAttr("class").hide();
-          }
-        });
-    }
-
-    function testEmailAddress()
-    {
-      let messageArea = $("#messageArea");
-      let emailAddressPattern = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
-        
-        $("#emailAddress").on("blur", function()
-        {
-          if(!emailAddressPattern.test($(this).val()))
-          {
-            $(this).trigger("focus").trigger("select");
-            messageArea.show().addClass("alert alert-danger").text("Please enter a valid Email Address.");
-          }
-          else
-          {
-              messageArea.removeAttr("class").hide();
-          }
-        });
-    }
-
-    function formValidation()
-    {
-      testFullName();
-      testContactNumber();
-      testEmailAddress();
-    }
-
     function displayContact()
     {
-      // form validation
-      formValidation();
+      let messageArea = $("#messageArea").hide();
+
+        // form validation
+        $("#fullName").on("blur", function()
+        {
+          if($(this).val().length < 2)
+          {
+            $(this).trigger("focus").trigger("select");
+            messageArea.show().addClass("alert alert-danger").text("Please enter an appropriate Name");
+          }
+          else
+          {
+              messageArea.removeAttr("class").hide();
+          }
+        });
 
         $("#sendButton").on("click", (event)=> 
         {
@@ -152,9 +100,7 @@
 
             if(contact.serialize())
             {
-              let key = contact.FullName.substring(0, 1) + Date.now();
-
-              localStorage.setItem(key, contact.serialize());
+              localStorage.setItem((localStorage.length + 1).toString(), contact.serialize());
             }
           }
         });
@@ -169,8 +115,6 @@
         let data = "";
 
         let keys = Object.keys(localStorage);
-         
-        let index = 1;
 
         for (const key of keys) 
         {
@@ -180,105 +124,33 @@
           contact.deserialize(contactData);
 
           data += `<tr>
-          <th scope="row" class="text-center">${index}</th>
+          <th scope="row">${key}</th>
           <td>${contact.FullName}</td>
           <td>${contact.ContactNumber}</td>
           <td>${contact.EmailAddress}</td>
           <td class="text-center"><button value="${key}" class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i> Edit</button></td>
           <td class="text-center"><button value="${key}" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i> Delete</button></td>
-          </tr>`;
-
-          index++;
+        </tr>`;
         }
 
         contactList.innerHTML = data;
 
+        //TODO - need to create an edit page
         $("button.edit").on("click", function(){
-          location.href = "edit.html#" + $(this).val();
+          console.log($(this).val());
          });
 
          $("button.delete").on("click", function(){
            if(confirm("Are you sure?"))
            {
             localStorage.removeItem($(this).val());
+            location.href = "contact-list.html"; // refresh the page
            }
-           location.href = "contact-list.html"; // refresh the page
-         });
-
-         $("#addButton").on("click", function() 
-         {
-          location.href = "edit.html";
          });
       }
     }
 
-    function displayEdit()
-    {
-      let key = location.hash.substring(1);
-
-      let contact = new core.Contact();
-
-      // check to ensure that the key is not empty
-      if(key != "")
-      {
-        // get contact info from localStorage
-        contact.deserialize(localStorage.getItem(key));
-
-        // display contact information in the form
-        $("#fullName").val(contact.FullName);
-        $("#contactNumber").val(contact.ContactNumber);
-        $("#emailAddress").val(contact.EmailAddress);
-      }
-      else
-      {
-        // modify the page so that it shows "Add Contact" in the header 
-        $("main>h1").text("Add Contact");
-        // modify edit button so that it shows "Add" as well as the appropriate icon
-        $("#editButton").html(`<i class="fas fa-plus-circle fa-lg"></i> Add`);
-      }
-
-      // form validation
-      formValidation();
-      
-     $("#editButton").on("click", function() 
-        {
-            // check to see if key is empty
-          if(key == "")
-          {
-            // create a new key
-            key = contact.FullName.substring(0, 1) + Date.now();
-          }
-
-          // copy contact info from form to contact object
-          contact.FullName = $("#fullName").val();
-          contact.ContactNumber = $("#contactNumber").val();
-          contact.EmailAddress = $("#emailAddress").val();
-
-          // add the contact info to localStorage
-          localStorage.setItem(key, contact.serialize());
-
-          // return to the contact list
-          location.href = "contact-list.html";
-          
-        });
-   
-
-      $("#cancelButton").on("click", function()
-      {
-        // return to the contact list
-        location.href = "contact-list.html";
-      });
-    }
-
-    function displayLogin()
-    {
-
-    }
-
-    function displayRegister()
-    {
-
-    }
+     
 
     function Start()
     {
@@ -303,15 +175,6 @@
             break;
           case "Contact-List":
             displayContactList();
-            break;
-          case "Edit":
-            displayEdit();
-            break;
-          case "Login":
-            displayLogin();
-          break;
-          case "Register":
-            displayRegister();
           break;
         }
         
